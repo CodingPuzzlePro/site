b082ac45fa6a1a1363a3fa96a002d1dbda47c38a
AbuSalah <mr_abusalah@hotmail.com>
2024-03-18 20:04:33 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const dataset = new Map([
+    ["1", "https://codeforces.com/contest/1941/problem/A"]
+]);
+
+
+submitInput = document.getElementById('submit-button')
+
+submitInput.addEventListener('click', function() {
+    link = document.getElementById('link-number-input')
+    if(link.value.length) {
+        linkFromDataset = dataset.get(link.value)
+        if(linkFromDataset) {
+            window.open(linkFromDataset)
+        } else {
+            alert('The number is not exist !')
+        }
+    } else {
+        alert('Enter valid number please')
+    }
+});
